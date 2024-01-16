96bb3a34aede0bc90b0e14b87c0d2de762f234a7
PrimaryV1C <petrovictorelias@gmail.com>
2024-01-16 16:11:29 +0100

    addded auto increment

--- app/src/main/java/com/cc221020/ccl3/data/Goal.kt
+++ app/src/main/java/com/cc221020/ccl3/data/Goal.kt
@@ -2,9 +2,9 @@ package com.cc221020.ccl3.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-@Entity(tableName = "Goals")
+@Entity(tableName = "Goal")
 data class Goal(
-    @PrimaryKey val id : Int = 0,
+    @PrimaryKey(autoGenerate = true) val id : Int = 0,
     var title : String,
     var completed : Boolean
 )
