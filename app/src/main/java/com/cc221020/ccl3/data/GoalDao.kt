76e067a42fb54daa317984ee0d75a8eea3020c85
PrimaryV1C <petrovictorelias@gmail.com>
2024-01-16 13:42:20 +0100

    Added database interfaces

---
+++ app/src/main/java/com/cc221020/ccl3/data/GoalDao.kt
@@ -0,0 +1,27 @@
+package com.cc221020.ccl3.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GoalDao {
+
+    @Insert
+    suspend fun insertGoal(goal: Goal)
+
+    @Update
+    suspend fun updateGoal(goal: Goal)
+
+    @Delete
+    suspend fun deleteGoal(goal: Goal)
+
+    @Query("SELECT * FROM Goals")
+    fun getGoal(): Flow<List<Goal>>
+
+    @Query("DELETE FROM Goals")
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/com/cc221020/ccl3/data/TodoDao.kt
@@ -0,0 +1,27 @@
+package com.cc221020.ccl3.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+
+    @Insert
+    suspend fun insertTodoItem(item: TodoItem)
+
+    @Update
+    suspend fun updateTodoItem(item: TodoItem)
+
+    @Delete
+    suspend fun deleteTodoItem(item: TodoItem)
+
+    @Query("SELECT * FROM TodoItems")
+    fun getTodoItem(): Flow<List<TodoItem>>
+
+    @Query("DELETE FROM TodoItems")
+    suspend fun deleteAll()
+}
