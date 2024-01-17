ebf2feae39b9239dd95e323767655b9f7651cdce
Fridolin Christian Hurra <103061952+lervus@users.noreply.github.com>
2024-01-17 01:39:35 +0100

    Avatar image

--- app/src/main/java/com/cc221020/ccl3/view/Avatar.kt
+++ app/src/main/java/com/cc221020/ccl3/view/Avatar.kt
@@ -2,6 +2,7 @@ package com.cc221020.ccl3.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.cc221020.ccl3.MainViewModel
 import com.cc221020.ccl3.R
@@ -28,6 +31,11 @@ fun Avatar(navController: NavController, mainViewModel: MainViewModel){
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(text = "This is the Avatar Page")
+        Image(
+            modifier = Modifier,
+            painter = painterResource(id = R.drawable.ic_action_name),
+            contentDescription = stringResource(id = R.string.avatar_image)
+        )
         Row {
             Button(onClick = { navController.navigate("me") }) {
                 Text(text = "Me")
