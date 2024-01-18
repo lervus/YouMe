4f6956434c7c610897fc9a7dd75b305e281ff562
PrimaryV1C <petrovictorelias@gmail.com>
2024-01-18 10:11:23 +0100

    added todo list element

---
+++ app/src/main/java/com/cc221020/ccl3/ui/components/TodoList.kt
@@ -0,0 +1,46 @@
+package com.cc221020.ccl3.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.cc221020.ccl3.MainViewModel
+import com.cc221020.ccl3.view.GoalView
+
+@Composable
+fun TodoList(navController: NavController ,mainViewModel: MainViewModel, goalId: Int){
+
+    mainViewModel.getTodos(goalId);
+    val state = mainViewModel.mainViewState.collectAsState()
+
+    LazyColumn(
+        modifier = Modifier
+            .padding(top = 50.dp)
+    ){
+
+        item{ Text(text = "Your Todos:" ) }
+        items(state.value.todos){
+            Column {
+                Text(text = it.title)
+            }
+            IconButton(onClick = { mainViewModel.deleteTodo(it) }) {
+                Icon(Icons.Default.Delete, "Delete", modifier = Modifier.size(35.dp))
+            }
+        }
+    }
+}
