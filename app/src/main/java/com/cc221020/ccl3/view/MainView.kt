bdc9e5c650c56a3ff614f37f8695cf4dcfad202e
PrimaryV1C <petrovictorelias@gmail.com>
2024-01-18 10:11:16 +0100

    Navigation to todo window

--- app/src/main/java/com/cc221020/ccl3/view/MainView.kt
+++ app/src/main/java/com/cc221020/ccl3/view/MainView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,6 +14,8 @@ import com.cc221020.ccl3.MainViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(mainViewModel: MainViewModel){
+
+    val state = mainViewModel.mainViewState.collectAsState()
 
     val navController = rememberNavController()
     Scaffold(){
@@ -25,6 +28,11 @@ fun MainView(mainViewModel: MainViewModel){
             composable("you"){ YouView(navController, mainViewModel) }
             composable( "avatar"){ Avatar(navController, mainViewModel) }
             composable( "enter"){ Enter(navController) }
+            composable("goalView/{goalId}"){ backStackEntry ->
+                val goalId = backStackEntry.arguments?.getString("goalId")
+                val goal = state.value.goals.firstOrNull { it.id.toString() == goalId}
+                if(goal != null){ GoalView(navController, mainViewModel, goal) }
+            }
         }
     }
 }
