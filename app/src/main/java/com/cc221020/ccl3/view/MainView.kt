b306fafc0f5b4369356121f6c703868bfabc876a
Fridolin Christian Hurra <103061952+lervus@users.noreply.github.com>
2024-01-22 17:18:07 +0100

    skin selection xp based

--- app/src/main/java/com/cc221020/ccl3/view/MainView.kt
+++ app/src/main/java/com/cc221020/ccl3/view/MainView.kt
@@ -1,24 +1,15 @@
 package com.cc221020.ccl3.view
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.cc221020.ccl3.MainViewModel
-import com.cc221020.ccl3.ui.components.BackButton
 import com.cc221020.ccl3.ui.components.InfoPopup
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,12 +22,11 @@ fun MainView(mainViewModel: MainViewModel) {
         NavHost(
             navController = navController,
             modifier = Modifier.padding(it),
-            startDestination = "enter"
+            startDestination = "avatar"
         ) {
             composable("me") { MeView(navController) }
             composable("you") { YouView(navController, mainViewModel) }
             composable("avatar") { Avatar(navController, mainViewModel) }
-            composable("enter") { Enter(navController) }
             composable("goalView/{goalId}") { backStackEntry ->
                 val goalId = backStackEntry.arguments?.getString("goalId")
                 val goal = state.value.goals.firstOrNull { it.id.toString() == goalId }
@@ -44,7 +34,7 @@ fun MainView(mainViewModel: MainViewModel) {
                     GoalView(navController, mainViewModel, goal)
                 }
             }
-            composable("settings"){ SettingView(navController, mainViewModel)}
+            composable("settings") { SettingView(navController, mainViewModel) }
         }
         InfoPopup(mainViewModel)
     }
